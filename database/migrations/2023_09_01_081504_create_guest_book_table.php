918c61745c317832c628297256575b34f00759b2
Rifkichairil <rfkchrl.03@gmail.com>
2023-09-01 15:23:00 +0700

    [ Databse ] Migrate tb guest_book

--- database/migrations/2023_09_01_081504_create_guest_book_table.php
+++ database/migrations/2023_09_01_081504_create_guest_book_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('guest_book', function (Blueprint $table) {
             $table->id();
-            $table->string('name',)
+            $table->string('name', 150);
+            $table->text('address');
+            $table->string('phone',25);
+            $table->text('comment');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
